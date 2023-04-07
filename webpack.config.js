a867374dee9aca3814a57aec7c4dc2244dad474c
DmitryVasylenko0808 <dvasilenko1999@gmail.com>
2023-04-07 15:32:56 +0300

    fix icons

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
     },
     output: {
         filename: '[name].[contenthash].js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist/js')
     },
     plugins: [
         new HTMLWebpackPlugin({
